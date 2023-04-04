b04a428b1d56e80080fe8f2fdfb9e9eabdcc354a
Usuario <Usuario@192.168.1.179>
2023-04-04 16:34:37 -0600

    IndexController y pequeña modificacion del index

--- src/main/java/com/ProyectoDesarrolloWeb/Controller/IndexController.java
+++ src/main/java/com/ProyectoDesarrolloWeb/Controller/IndexController.java
@@ -1,9 +1,49 @@
 package com.ProyectoDesarrolloWeb.Controller;
 
+import com.ProyectoDesarrolloWeb.domain.Usuario;
+import com.ProyectoDesarrolloWeb.service.UsuarioService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
 /**
  *
  * @author ADMIN
  */
+@Slf4j
+@Controller
 public class IndexController {
     
+    @Autowired
+    UsuarioService usuarioService;
+    
+    @GetMapping("/")
+    public String inicio(Model model) {
+        log.info("Ahora utilizando MVC");
+        
+        //var usuarios = usuariosDao.findAll();
+        var usuarios= usuarioService.getUsuarios();
+        model.addAttribute("usuarios", usuarios);
+        return "index";
+    }
+    
+    @GetMapping("/nuevoUsuario")
+    public String nuevoUsuario(Usuario usuario){
+        return "modificarUsuario";
+    }
+    
+    @PostMapping("/guardarUsuario")
+    public String guardarUsuario(Usuario usuario){
+        usuarioService.save(usuario);
+        return "redirect:/";
+    }
+    
+    @GetMapping("/eliminarUsuario/{idUsuario}")
+    public String eliminarUsuario(Usuario usuario){
+        usuarioService.delete(usuario);
+        return "redirect:/";
+    }
 }
